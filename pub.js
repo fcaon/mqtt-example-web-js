fb509e6137996340581939ba2ea332ed4c78ab35
Fabiano <fabiano.caon@senairs.org.br>
2024-03-05 16:06:31 -0300

    Ajustes diversos

--- pub.js
+++ pub.js
@@ -1,3 +1,6 @@
+// Only nodeJS
+// From the shell command line run "node pub.js"
+
 var mqtt = require ('mqtt')
 var client = mqtt.connect('mqtt://test.mosquitto.org:1883')
 var topic = 'caon/teste/1'
@@ -9,7 +12,3 @@ client.on('connect', () => {
     client.publish(topic, message, {qos: 0, retain: false})
     console.log('Mensagem Enviada', message)
 })
-
-
-
-//funciona pelo nodeJS
--- sub.js
+++ sub.js
@@ -1,3 +1,6 @@
+// Only for NodeJS
+// From the shell command line run "node sub.js"
+
 var mqtt = require ('mqtt')
 var client = mqtt.connect('mqtt://test.mosquitto.org:1883')
 var topic = 'caon/#'
@@ -8,13 +11,9 @@ client.on('message', (topic, message, packet) => {
     message = message.toString()
     console.log(message)
     console.log(topic)
-    //console.log(packet.qos)
 })
 
 client.on('connect', () => {
     client.subscribe(topic)
 
 })
-
-
-//Funciona pelo NodeJSs
